8b50b2e670493f8003ebc74df31d11fabd0f584f
thalyta vitoria <thalyta.santos@escolar.ifrn.edu.br>
2021-02-27 22:27:44 -0300

    Iniciei a primeira parte da atividade PSI

---
+++ application/views/listar_gastos.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>lista de gastos de fevereiro</h1>
+    <table>
+        <tr>
+            <td>Data:</td>
+            <td>Descrição:</td>
+            <td>Valor:</td>
+        </tr>
+        <tr>
+            <td>05/02/2021</td>
+            <td>Chocolate</t/d>
+            <td>R$4000</td>
+        </tr>
+        <tr>
+            <td>06/02/2021</td>
+            <td>Cachaça cabaré</td>
+            <td>R$10000</td>
+        </tr>
+        <tr>
+            <td>07/02/2021</td>
+            <td>Batata frita</td>
+            <td>R$15000</td>
+        </tr>
+    </table>
+</body>
+</html>
